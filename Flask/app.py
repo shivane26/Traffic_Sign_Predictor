755b1c28f085eff4fbef684e7d6fdcc0e050aa0a
shivane26 <166207979+shivane26@users.noreply.github.com>
2024-11-14 14:55:33 +0530

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -96,4 +96,6 @@ def process_video(video_path):
     return processed_video_path
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
